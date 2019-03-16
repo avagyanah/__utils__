743a85b52a03f70f85243d5386394ce8974cdb33
Ashot Avagyan <avagyanah@gmail.com>
2019-03-17 00:43:27 +0400

    replacing Math.random

--- array/shuffle.js
+++ array/shuffle.js
@@ -1,6 +1,7 @@
 const { isEqual } = require("./isEqual");
 const { random } = require("../math/random");
 const { swap } = require("./swap");
+const { sample } = require("./sample");
 
 function shuffle(arr, noRepeat = false) {
   const origin = noRepeat && Array.from(arr);
@@ -13,7 +14,7 @@ function shuffle(arr, noRepeat = false) {
 
 function _shuffle(arr) {
   for (let i = 0; i < arr.length - 1; ++i) {
-    const rndIndex = Math.floor(Math.random() * arr.length);
+    const rndIndex = random(0, arr.length - 1);
     const deleted = arr.splice(rndIndex, 1)[0];
     arr.push(deleted);
   }
--- index.js
+++ index.js
@@ -6,3 +6,6 @@ const { sample } = require("./array/sample");
 const { sampleSize } = require("./array/sampleSize");
 const { shuffle } = require("./array/shuffle");
 const { swap } = require("./array/swap");
+const a = [1, 2];
+shuffle(a);
+console.warn(``, a);
