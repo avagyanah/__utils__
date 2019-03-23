dc1f006bc35a376f013658631bfcf4d5c0a90292
Ashot Avagyan <avagyanah@gmail.com>
2019-03-23 15:37:49 +0400

    minor

--- array/swap.js
+++ array/swap.js
@@ -1,9 +1,9 @@
-function swap(arr, i1, i2) {
-  const l = arr.length;
+function swap(array, i1, i2) {
+  const l = array.length;
   if (i1 < 0 || i2 < 0 || i1 > l - 1 || i2 > l - 1) {
     return new Error("Incorrect arguments: index1 or index2 doesn't fall in range");
   }
-  const copy = Array.from(arr);
+  const copy = Array.from(array);
   [copy[i1], copy[i2]] = [copy[i2], copy[i1]];
   return copy;
 }
