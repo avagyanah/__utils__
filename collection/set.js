c7e88a51e50bb324e0a41412bceb2cec020c040a
Ashot Avagyan <avagyanah@gmail.com>
2019-03-30 14:42:21 +0400

    delete from set

--- collection/set.js
+++ collection/set.js
@@ -1,39 +1,46 @@
+const { remove } = require("../array/remove");
+
 function Set() {
-  this.__values__ = [];
+  this.__entries__ = [];
 }
 
 Set.prototype.add = function(value) {
-  const existingIndex = this.__values__.indexOf(value);
+  const existingIndex = this.__entries__.indexOf(value);
   const isUnique = existingIndex === -1;
 
   if (isUnique) {
-    this.__values__.push(value);
+    this.__entries__.push(value);
   } else {
-    this.__values__[existingIndex] = value;
+    this.__entries__[existingIndex] = value;
   }
 };
 
+Set.prototype.delete = function(key) {
+  const index = this.__entries__.indexOf(key);
+  this.__entries__ = remove(this.__entries__, index);
+};
+
 Set.prototype.clear = function() {
-  this.__values__.length = 0;
+  this.__entries__.length = 0;
 };
 
 Set.prototype.forEach = function(callback) {
-  this.__values__.forEach(value => callback(value));
+  this.__entries__.forEach(value => callback(value));
 };
 
 Set.prototype.has = function(key) {
-  return this.__values__.indexOf(key) !== -1;
+  return this.__entries__.indexOf(key) !== -1;
 };
 
 Object.defineProperty(Set.prototype, "size", {
   get: function() {
-    return this.__values__.length;
+    return this.__entries__.length;
   }
 });
 
 Object.defineProperty(Set.prototype, "values", {
   get: function() {
-    return this.__values__;
+    return this.__entries__;
   }
 });
 
